3f118f09e0411e8676bbf9b732b0544dfdcce991
Yeshua Amador Chiliquinga Amaya <132861238+YeshuaChiliquingaAmaya@users.noreply.github.com>
2023-09-15 18:49:04 -0500

    Add all versions of text_Loop
    update all files

---
+++ Joke/forLoopV4.py
@@ -0,0 +1,35 @@
+import time
+import tkinter as tk
+from tkinter import simpledialog
+import keyboard
+
+def send_message():
+    message = entry.get("1.0", "end-1c")  # Obtiene el texto del cuadro de texto
+    number_value = simpledialog.askinteger("Input", "Enter the number of times to send the message:")
+
+    if message and number_value:
+        time.sleep(2)
+
+        for i in range(number_value):
+            keyboard.write(message)
+            keyboard.press_and_release('Enter')
+
+# Configura la ventana
+root = tk.Tk()
+root.title("Message Sender")
+
+# Crea un cuadro de texto
+entry = tk.Text(root, height=10, width=50)
+entry.pack(padx=10, pady=10)
+
+# Carga automáticamente el contenido de "ascii_art.txt" en el cuadro de texto, respetando los espacios
+with open("ascii_art.txt", "r") as file:
+    ascii_art = file.read()
+    entry.insert("1.0", ascii_art)
+
+# Crea un botón para enviar el mensaje
+send_button = tk.Button(root, text="Send Message", command=send_message)
+send_button.pack(pady=10)
+
+# Ejecuta la interfaz gráfica
+root.mainloop()
